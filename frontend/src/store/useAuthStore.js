647fe07b8d1a1e64e0bcce1e3e2af4eb2f347889
SlayZ121 <slayz9168@gmail.com>
2025-05-23 14:04:29 +0530

    LAST HOPE

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -5,7 +5,7 @@ import { useChatStore } from "./useChatStore.js";
 import { io } from "socket.io-client";
 
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5001" : "/api";
+  import.meta.env.MODE === "development" ? "http://localhost:5001" : "/";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
@@ -88,8 +88,6 @@ export const useAuthStore = create((set, get) => ({
     if (!authUser || get().socket?.connected) return;
 
     const socket = io(BASE_URL, {
-      forceNew: true,
-
       query: {
         userId: authUser._id,
       },
