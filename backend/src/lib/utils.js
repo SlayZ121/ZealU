b1ea1832780d05f4bef742aed5d191116649ec2c
SlayZ121 <slayz9168@gmail.com>
2025-05-23 12:41:35 +0530

    Fixing socket.io issue

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,6 +1,5 @@
 import jwt from "jsonwebtoken";
 export const generateJWT = (userId, res) => {
-  console.log(process.env.JWT_SECRET);
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
